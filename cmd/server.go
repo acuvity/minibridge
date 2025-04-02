191a68833a8961f371e939bcb0bb1745bca70370
primalmotion <primalmotion@pm.me>
2025-04-02 18:10:57 -0400

    add profiling server

--- cmd/server.go
+++ cmd/server.go
@@ -24,6 +24,8 @@ func init() {
 	Server.Flags().StringSlice("mcp-arg", nil, "List of argument to pass to the MCP server")
 	Server.Flags().Bool("health-enable", false, "If set, start a health server for production deployments")
 	Server.Flags().String("health-listen", ":8080", "Listen address of the health server")
+	Server.Flags().Bool("profiling-enable", false, "If set, enable profiling server")
+	Server.Flags().String("profiling-listen", ":6060", "Listen address of the health server")
 }
 
 // Server is the cobra command to run the server.
@@ -51,6 +53,8 @@ var Server = &cobra.Command{
 		clientCAPath := viper.GetString("client-ca")
 		healthEnabled := viper.GetBool("health-enable")
 		healthListen := viper.GetString("health-listen")
+		profilingEnabled := viper.GetBool("profiling-enable")
+		profilingListen := viper.GetString("profiling-listen")
 
 		tlsConfig := &tls.Config{}
 		var hasTLS bool
@@ -87,24 +91,30 @@ var Server = &cobra.Command{
 			tlsConfig = nil
 		}
 
-		mcpServer := mcp.Server{
-			Command: args[0],
-			Args:    args[1:],
+		metricsManager := bahamut.NewPrometheusMetricsManager()
+		opts := []bahamut.Option{}
+
+		if healthEnabled && healthListen != "" {
+			opts = append(
+				opts,
+				bahamut.OptHealthServer(healthListen, nil),
+				bahamut.OptHealthServerMetricsManager(metricsManager),
+			)
 		}
 
-		slog.Info("WS Server configured", "tls", hasTLS, "listen", listen)
-		slog.Info("MCP Server configured", "command", mcpServer.Command, "args", mcpServer.Args)
+		if profilingEnabled && profilingListen != "" {
+			opts = append(opts, bahamut.OptProfilingLocal(profilingListen))
+		}
 
-		metricsManager := bahamut.NewPrometheusMetricsManager()
-
-		proxy := mcp.NewWSProxy(listen, tlsConfig, mcpServer)
-		proxy.Start(cmd.Context())
-
-		opts := []bahamut.Option{}
-		if healthEnabled && healthListen != "" {
-			opts = append(opts, bahamut.OptHealthServer(healthListen, nil), bahamut.OptHealthServerMetricsManager(metricsManager))
+		if len(opts) > 0 {
 			go bahamut.New(opts...).Run(cmd.Context())
 		}
+
+		mcpServer := mcp.Server{Command: args[0], Args: args[1:]}
+		slog.Info("WS Server configured", "tls", hasTLS, "listen", listen)
+		slog.Info("MCP Server configured", "command", mcpServer.Command, "args", mcpServer.Args)
+		proxy := mcp.NewWSProxy(listen, tlsConfig, mcpServer)
+		proxy.Start(cmd.Context())
 
 		<-cmd.Context().Done()
 
