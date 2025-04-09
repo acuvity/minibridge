21a746986b65c6cbca9906bbf8ea086cfbde33a6
CyrilPeponnet <cyril@peponnet.fr>
2025-04-09 10:28:30 -0700

    client: handle process exit properly

--- pkgs/client/client.go
+++ pkgs/client/client.go
@@ -15,6 +15,8 @@ type stdioClient struct {
 	stdin  io.WriteCloser
 	stdout io.ReadCloser
 	stderr io.ReadCloser
+
+	cancel context.CancelFunc
 }
 
 // NewStdio returns a Client communicating through stdio.
@@ -38,6 +40,10 @@ func NewStdio(srv MCPServer) (Client, error) {
 		return nil, fmt.Errorf("unable to create stderr pipe: %w", err)
 	}
 
+	if err := cmd.Start(); err != nil {
+		return nil, fmt.Errorf("unable to start command: %w", err)
+	}
+
 	client := &stdioClient{
 		cmd:    cmd,
 		stdin:  stdin,
@@ -45,14 +51,15 @@ func NewStdio(srv MCPServer) (Client, error) {
 		stdout: stdout,
 	}
 
-	if err := cmd.Start(); err != nil {
-		return nil, fmt.Errorf("unable to start command: %w", err)
-	}
+	go client.monitorProcess()
 
 	return client, nil
 }
 
 func (c *stdioClient) Start(ctx context.Context) (in chan []byte, out chan []byte, err chan []byte) {
+
+	ctx, cancel := context.WithCancel(ctx)
+	c.cancel = cancel
 
 	in = make(chan []byte, 1024)
 	go c.readRequests(ctx, in)
@@ -66,19 +73,34 @@ func (c *stdioClient) Start(ctx context.Context) (in chan []byte, out chan []byt
 	return in, out, err
 }
 
+func (c *stdioClient) monitorProcess() {
+
+	err := c.cmd.Wait()
+	if err != nil {
+		slog.Error("Command crashed", "cmd", c.cmd.Args[0], "err", err)
+	} else {
+		slog.Warn("Command exited normally", "cmd", c.cmd.Args[0])
+	}
+
+	if c.cancel != nil {
+		c.cancel()
+	}
+	os.Exit(c.cmd.ProcessState.ExitCode())
+}
+
 func (c *stdioClient) readRequests(ctx context.Context, ch chan []byte) {
 
 	for {
-
 		select {
-
 		case <-ctx.Done():
 			_ = c.stdin.Close()
 			return
-
 		case data := <-ch:
 			if _, err := c.stdin.Write(data); err != nil {
-				slog.Error("Unable to write data to stdin", "err", err)
+				if ctx.Err() == nil {
+					slog.Error("Unable to write data to stdin", "err", err)
+				}
+				return
 			}
 		}
 	}
@@ -87,29 +109,19 @@ func (c *stdioClient) readRequests(ctx context.Context, ch chan []byte) {
 func (c *stdioClient) readResponses(ctx context.Context, ch chan []byte) {
 
 	stdout := bufio.NewReader(c.stdout)
-
 	for {
-
-		select {
-
-		case <-ctx.Done():
-			_ = c.stdout.Close()
+		data, err := stdout.ReadBytes('\n')
+		if err != nil {
+			if ctx.Err() != nil || err == io.EOF {
+				return
+			}
+			slog.Error("Unable to read response from stdout", "err", err)
 			return
-
-		default:
-
-			data, err := stdout.ReadBytes('\n')
-			if err != nil {
-				if err != io.EOF {
-					slog.Error("Unable to read response from stdout", "err", err)
-				}
-				continue
-			}
-
-			select {
-			case ch <- data:
-			default:
-			}
+		}
+		select {
+		case ch <- data:
+		case <-ctx.Done():
+			return
 		}
 	}
 }
@@ -117,29 +129,19 @@ func (c *stdioClient) readResponses(ctx context.Context, ch chan []byte) {
 func (c *stdioClient) readErrors(ctx context.Context, ch chan []byte) {
 
 	stderr := bufio.NewReader(c.stderr)
-
 	for {
-
-		select {
-
-		case <-ctx.Done():
-			_ = c.stderr.Close()
+		line, err := stderr.ReadBytes('\n')
+		if err != nil {
+			if ctx.Err() != nil || err == io.EOF {
+				return
+			}
+			slog.Error("Unable to read error response from stderr", "err", err)
 			return
-
-		default:
-
-			line, err := stderr.ReadBytes('\n')
-			if err != nil {
-				if err != io.EOF {
-					slog.Error("Unable to read response from stdout", "err", err)
-				}
-				continue
-			}
-
-			select {
-			case ch <- line:
-			default:
-			}
+		}
+		select {
+		case ch <- line:
+		case <-ctx.Done():
+			return
 		}
 	}
 }
