ebb1bd33ff9e4b3269c0547b70cc6f8b2404e27a
primalmotion <primalmotion@pm.me>
2025-04-16 12:30:00 -0400

    example/policer: add a bit more info and colors

--- policers/example/policer.py
+++ policers/example/policer.py
@@ -1,39 +1,65 @@
 #!/bin/python
+"""
+This is an example implementation for a policer
+"""
 
 import json
+from termcolor import colored
 from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/police", methods=['POST'])
+FORBIDDEN_METHODS = ["printEnv"]
+
+
+@app.route("/police", methods=["POST"])
 def police():
+    """handles /police"""
+
     req = request.get_json()
+    agent = req["agent"]
+    mcp = req["mcp"]
 
-
+    print()
     print("---")
-    print("Type: %s" % req['type'])
-    print("Headers: %s" % request.headers)
+    print(
+        colored(
+            f"Type: ${req['type']}", "green" if req["type"] == "input" else "yellow"
+        )
+    )
+    print(colored(f"Agent: ${agent['userAgent']} ${agent['remoteAddr']}", "blue"))
+    print()
+    print(colored(f"${request.headers}", "dark_grey"))
     print(json.dumps(req, sort_keys=True, indent=4))
 
     # If there is no agent token, we refuse the call.
     # Of course, this is an example. IRL, you will validate that token.
-    if req['token'] == "":
-        print("REJECTED: no token")
-        return json.dumps({
-            "decision": "deny",
-            "reasons": ["you have not passed any token"],
-        })
+    if req["agent"]["token"] == "":
+        print(colored("DENIED: no token", "red"))
+        return json.dumps(
+            {
+                "decision": "deny",
+                "reasons": ["you have not passed any token"],
+            }
+        )
 
     # As an example, we forbid user to use list_directory tools. Why? why not?
-    if req['type'] == 'input':
-        mcp = req['mcp']
-        if mcp['method'] == "tools/call" and mcp['params']['name'] == "list_directory":
-            return json.dumps({
-                "decision": "deny",
-                "reasons": ["you cannot list directories"],
-            })
+    if req["type"] == "input":
+        if (
+            mcp["method"] == "tools/call"
+            and "name" in mcp["params"]
+            and mcp["params"]["name"] in FORBIDDEN_METHODS
+        ):
+            denied_msg = (
+                f"forbidden method call ${mcp['params']['name']} ${mcp['method']}"
+            )
+            print(colored(f"DENIED: ${denied_msg}", "red"))
+            return json.dumps({"decision": "deny", "reasons": [denied_msg]})
+
+    print(colored("ALLOWED", "green"))
 
     # otherwise we allow everything
     return json.dumps({"decision": "allow"})
+
 
 app.run(port=5000)
